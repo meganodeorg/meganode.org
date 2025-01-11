b9e47cc062a7c896dd254d0882c4e0b12c76591f
phuocphan <phuocphanplus@gmail.com>
2025-01-11 22:00:07 +0700

    feat: Redesign website with modern glassmorphic UI

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
